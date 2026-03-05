e16fdec7f7a6c961b08546c723114319193c0cf5
Biswajeet Mohapatra <secondarybiswajeet@gmail.com>
2026-03-06 00:02:40 +0530

    Time: 1 ms (49.52%), Space: 13.6 MB (6.97%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.c
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -6,51 +6,25 @@
  * };
  */
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
-    int carry;
-    struct ListNode *head = (struct ListNode*)malloc(sizeof(struct ListNode)), *end = NULL;
-    int sum = (l1->val + l2->val);
+    struct ListNode dummy, *end = &dummy;
+    int carry = 0;
 
-    head->val = (sum >= 10) ? sum % 10 : sum;
-    carry = (sum >= 10) ? sum / 10 : 0;
-    
-    head->next = NULL;
-    end = head;
-    l1 = l1->next;
-    l2 = l2->next;
-
-    while (l1 != NULL || l2 != NULL) {
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+        int val1 = (l1 != NULL) ? l1->val : 0;
+        int val2 = (l2 != NULL) ? l2->val : 0;
+        int sum = (val1 + val2 + carry);
+        
         struct ListNode *newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
-        sum = 0;
-        
-        if (l1 == NULL) {
-            sum += (l2->val + carry);
-        } else if (l2 == NULL) {
-            sum += (l1->val + carry);
-        } else {
-            sum += (l1->val + l2->val + carry);
-        }
-        
-        if (sum >= 10) {
-            newNode->val = sum % 10;
-            carry = sum / 10;
-        } else {
-            newNode->val = sum;
-            carry = 0;
-        }
-        
+        newNode->val = sum % 10;
         newNode->next = NULL;
+
         end->next = newNode;
         end = newNode;
-        l1 = (l1 == NULL) ? NULL : l1->next;
-        l2 = (l2 == NULL) ? NULL : l2->next;
+        carry = sum / 10;
+        
+        l1 = (l1 != NULL) ? l1->next : NULL;
+        l2 = (l2 != NULL) ? l2->next : NULL;
     }
 
-    if (carry != 0) {
-        struct ListNode *newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
-        newNode->val = carry;
-        newNode->next = NULL;
-        end->next = newNode;
-    }
-
-    return head;
+    return dummy.next;
 }

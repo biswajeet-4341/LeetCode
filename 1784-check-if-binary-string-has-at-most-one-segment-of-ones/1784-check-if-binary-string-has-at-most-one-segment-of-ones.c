4333e619251a88980c877322dd2790fdfcb5e174
Biswajeet Mohapatra <secondarybiswajeet@gmail.com>
2026-03-07 00:19:26 +0530

    Time: 0 ms (100%), Space: 8.6 MB (5.56%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.c
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.c
@@ -1,17 +1,3 @@
 bool checkOnesSegment(char* s) {
-    int i = 0;
-
-    while (i < strlen(s)) {
-        if (s[i] == '0') {
-            while (i < strlen(s) && s[i] == '0') {
-                i++;
-            }
-            bool retn = (i >= strlen(s)) ? true : (s[i] == '1') ? false : true;
-            return retn;
-        }
-
-        i++;
-    }
-
-    return true;
+    return (strstr(s, "01") == NULL);
 }
